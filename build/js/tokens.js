a5f5d20326d025cfdbc183c81eece8e3a64a25eb
lucassodresa <lucassodresa@users.noreply.github.com>
2022-10-09 16:21:08 +0000

    style: Update design tokens

--- build/js/tokens.js
+++ build/js/tokens.js
@@ -1,13 +1,17 @@
 /**
  * Do not edit directly
- * Generated on Sun, 09 Oct 2022 13:38:45 GMT
+ * Generated on Sun, 09 Oct 2022 16:21:08 GMT
  */
 
 module.exports = {
+  "Sizes2": 8,
+  "Sizes4": 16,
+  "Sizes8": 32,
   "ColorWhite": "#ffffff",
   "ColorGray200": "#c1c1c4",
   "ColorGray500": "#50505c",
   "ColorGray800": "#1c1c1f",
   "ColorGray900": "#121214",
+  "ColorPurple400": "#9b74f3",
   "ColorPurple500": "#8b5cf6"
 };
