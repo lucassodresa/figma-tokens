527cbf33e179da7e4705895f1c437ecab5f6c3ff
lucassodresa <lucas.sodresa@gmail.com>
2022-10-09 15:30:25 +0100

    initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Sun, 09 Oct 2022 13:38:45 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#50505c",
+  "ColorGray800": "#1c1c1f",
+  "ColorGray900": "#121214",
+  "ColorPurple500": "#8b5cf6"
+};
